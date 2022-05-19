a4cf4baef70066b1a75b602339730e3f70be7425
oseias-romeiro <oseiasromeirom@gmail.com>
2022-05-18 21:08:41 -0300

    postgreSQL init

--- SistemaDeFuncionarios/src/database/PostgreSQLJDBC.java
+++ SistemaDeFuncionarios/src/database/PostgreSQLJDBC.java
@@ -6,6 +6,8 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -15,17 +17,34 @@ public class PostgreSQLJDBC {
     public PostgreSQLJDBC(){
     
         Connection con = null;
+        Statement stmt = null;
         
         try {
             Class.forName("org.postgresql.Driver");
             con = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    .getConnection("jdbc:postgresql://localhost:5432/sisfunc",
             "postgres", "qwer1234");
+            
+            con.setAutoCommit(false);
+            
+            System.out.println("Opened database successfully");
+        
+            stmt = con.createStatement();
+            String sql = "SELECT * FROM gestores;";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                System.out.println(rs.getString("Nome"));
+            }
+            
+            rs.close();
+            stmt.close();
+            con.close();
+        
         }catch (Exception e){
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
